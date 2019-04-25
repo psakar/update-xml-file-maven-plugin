e3ef7fc9268605ceb342f82f0e18172880e214d7
Roberto Oliveira <rguimara@redhat.com>
2019-04-25 13:45:52 +0200

    update help goal

--- src/main/java/org/wildfly/maven/plugins/updateXmlFile/HelpMojo.java
+++ src/main/java/org/wildfly/maven/plugins/updateXmlFile/HelpMojo.java
@@ -3,18 +3,23 @@ package org.wildfly.maven.plugins.updateXmlFile;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.INITIALIZE;
 import static org.wildfly.maven.plugins.updateXmlFile.Constants.HELP_GOAL;
 
+import javax.inject.Inject;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name = HELP_GOAL, defaultPhase = INITIALIZE, requiresProject = false)
 public class HelpMojo extends AbstractMojo {
 
+	@Inject
+	private MavenProject project;
 
 	public void execute() {
-		//FIXME replace version
-		//FIXME show readme ?
+		String version = project.getVersion();
+		String scm = project.getProperties().getProperty("scm.url");
 		getLog().info("To update xml file:");
-		getLog().info("   org.wildfly.maven.plugins:update-xml-file-maven-plugin:0.0.1.Final:update");
+		getLog().info("   org.wildfly.maven.plugins:update-xml-file-maven-plugin:" + version + ":update");
+		getLog().info("The README with all options can be found at: " + scm);
 	}
-
 }
